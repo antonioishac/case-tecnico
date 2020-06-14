ea97035a6f614d5c9812a5e768616f0756b14ce8
antonioishac <antonioishac@gmail.com>
2020-06-14 22:12:26 +0000

    Versão com o banco de dados postgresql

--- src/main/resources/db/migration/V0001__criacao-tabelas.sql
+++ src/main/resources/db/migration/V0001__criacao-tabelas.sql
@@ -1,22 +1,19 @@
-CREATE TABLE `tb_estado` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `sigla` VARCHAR(2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE tb_estado(
+  id serial PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  sigla VARCHAR(2) NOT NULL
+);
 
-CREATE TABLE `tb_banco` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `agencia` VARCHAR(20) NOT NULL,
-  `logradouro` VARCHAR(150) NOT NULL,
-  `bairro` VARCHAR(60),
-  `cidade` VARCHAR(50) NOT NULL,
-  `cep` VARCHAR(20),
-  `estado_id` BIGINT(20) NOT NULL,
-  `data_cadastro` DATETIME NOT NULL,
-  `data_atualizacao` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_banco_estado` (`estado_id`),
-  CONSTRAINT `fk_banco_estado` FOREIGN KEY (`estado_id`) REFERENCES `tb_estado` (`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE tb_banco(
+  id serial PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  agencia VARCHAR(20) NOT NULL,
+  logradouro VARCHAR(150) NOT NULL,
+  bairro VARCHAR(60),
+  cidade VARCHAR(50) NOT NULL,
+  cep VARCHAR(20),
+  estado_id integer NOT NULL,
+  data_cadastro timestamp NOT NULL,
+  data_atualizacao timestamp NOT NULL,
+  CONSTRAINT fk_banco_estado FOREIGN KEY (estado_id) REFERENCES tb_estado (id)
+);
